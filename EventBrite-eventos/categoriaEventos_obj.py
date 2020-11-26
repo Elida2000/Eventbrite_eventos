7ca0c9fcfd6d599f7a2f6d71577140a354d6e110
Elida2000 <72025704+Elida2000@users.noreply.github.com>
2020-11-25 20:02:02 -0600

    Delete categoriaEventos_obj.py

--- EventBrite-eventos/categoriaEventos_obj.py
+++
@@ -1,5 +0,0 @@
-class CategoriaEventosObj:
-    def __init__(self, idCategoriasEventos, nombreCat):
-        self.idCategoriasEventos = idCategoriasEventos
-        self.nombreCat = nombreCat
-        
